d17dcf0204b1490e252d94ab3162048402d59807
chadi@nsis.com.au <chadi@nsis.com.au>
2021-11-26 16:02:19 +1100

    Help description prettied up

--- main.py
+++ main.py
@@ -4,22 +4,19 @@ from proxmoxNodesAndVMs import proxmoxData
 
 class terminal:
     def main(self):
-        parser = argparse.ArgumentParser(description='/\/\/\/\/\/\/\/\ This script retrieves an inventory of nodes '
-                                                     'and vm\'s in a Proxmox cluster /\/\/\/\/\/\/\/\\')
-
+        parser = argparse.ArgumentParser(description='_______________/\/\/\/\/\/\/\/\ This script retrieves an inventory of nodes '
+                                                     'and vm\'s in a Proxmox cluster /\/\/\/\/\/\/\/\\_______________')
         parser.add_argument('-dns', '--dnsname', type=str, metavar='', required=True,
                             help='The Subject Alternate Name or DNS Name of the host/cluster (can be retrieved '
-                                 'from certificate under \'Subject Alternate Name\')\n')
+                                 'from certificate under \'Subject Alternate Name\')')
         parser.add_argument('-user', '--username', type=str, metavar='', required=True,
-                            help='The Username that will be used to sign in with\n')
+                            help='The Username that will be used to sign in with')
         parser.add_argument('-pwd', '--password', type=str, metavar='', required=True,
-                            help='Password credentials to authenticate user\n')
+                            help='Password credentials to authenticate user')
         parser.add_argument('-p', '--port', type=int, metavar='', required=True,
-                            help='The port that connection will be made to\n')
-        parser.add_argument('-c', '--csv', type=str, metavar='', required=False,
-                            help='Export data as a csv to this location\n')
-
-        parser.print_help()
+                            help='The port that connection will be made to')
+        parser.add_argument('-c', '--csv', type=str, metavar='', required=True,
+                            help='Export data as a csv to this location')
 
         args = parser.parse_args()
 
