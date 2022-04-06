615fde912e792c012171ec0293048b2a8a74e485
DoctaX <81501610+DoctaX@users.noreply.github.com>
2022-04-06 15:37:37 +1000

    Mistake refactored

--- thin_client_logic.py
+++ thin_client_logic.py
@@ -2,8 +2,11 @@ from proxmox_nodes_and_vms import Prox
 
 
 def logic(dns, user, pwd, port, file_type, file_path, skipSSL):
+    
     instance = Prox()
 
+    file_type = file_type.lower()
+    
     if not instance.is_valid_path(file_path):
         print("The file path you have entered is not valid")
         return
@@ -22,12 +25,12 @@ def logic(dns, user, pwd, port, file_type, file_path, skipSSL):
     nodes = instance.get_nodes()
     vms = instance.get_vms(nodes)
 
-    if file_type.lower() == 'csv':
+    if file_type == 'csv':
         rows, max_disks, max_ips, max_vlans = instance.format_CSV(vms)
         rows = instance.list_to_column(rows, max_disks, max_ips, max_vlans)
         instance.output_to_CSV_file(file_path, rows)
 
-    elif file_type.lower() == 'yaml':
+    elif file_type == 'yaml':
         data = instance.format_YAML(nodes, vms)
         instance.output_to_YAML_file(data, file_path)
 
